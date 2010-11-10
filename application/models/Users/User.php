890f9adc3b1250bd1e985d1e698efae44f3ec56a
Mark Swanson <markisacat@gmail.com>
2010-11-10 11:18:32 +0900

    Users Table - completed

--- application/models/Users/User.php
+++ application/models/Users/User.php
@@ -26,51 +26,6 @@ class Application_Model_Users_User extends Custom_Model_Abstract
 	public $reviewCount;
 	public $reviewAverageScore;
 	public $reviewTotalScore;
-	
-	
-//	public function setId($id) {
-//		$this->_id = (int) $id;
-//		return $this;
-//	}
-//	
-//	public function getId() {
-//		return $this->_id;
-//	}
-//	
-//	public function setUsername($username) {
-//		$this->_username = (string) $username;
-//		return $this;
-//	}
-//	
-//	public function getUsername() {
-//		return $this->_username;
-//	}
-//	
-//	public function setPassword($password) {
-//		$this->_password = (string) $password;
-//		return $this;
-//	}
-//	
-//	public function getPassword() {
-//		return $this->_password;
-//	}
-//	
-//	public function setRole($role) {
-//		$this->_role = (string) $role;
-//		return $this;
-//	}
-//	
-//	public function getRole() {
-//		return $this->_role;
-//	}
-//	
-//	public function setDateCreated($dateCreated) {
-//		$this->_dateCreated = $dateCreated;
-//		return $this;
-//	}
-//	
-//	public function getDateCreated() {
-//		return $this->_dateCreated;
-//	}
+
 }
 
