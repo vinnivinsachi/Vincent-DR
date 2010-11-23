345dca9c7d956029454f7bc70d2fd17d16315968
Vincent Zhang <vinzha@gmail.com>
2010-11-23 18:37:03 -0500

    productImage in progress workaround

--- application/models/mappers/Products/ProductImagesMapper.php
+++ application/models/mappers/Products/ProductImagesMapper.php
@@ -19,13 +19,29 @@ class Application_Model_Mapper_Products_ProductImagesMapper extends Custom_Model
 	} 
 	
 	public function fetchOrderNumber($imageTable, $sourceName, $sourceTypeTitle, $sourceTypeName, $sourceID){
-		$query = sprintf("select coalesce(max(imageOrder),0) + 1 from %s where sourceName = '%s' and sourceTypeTitle= '%s' and sourceTypeName = '%s' and sourceID='%s'", $imageTable, $sourceName, $sourceTypeTitle, $sourceTypeName, $sourceID);
+		
+		
+		//$query = sprintf("select coalesce(max(imageOrder),0) + 1 from %s where sourceName = '%s' and sourceTypeTitle= '%s' and sourceTypeName = '%s' and sourceID='%s'", $imageTable, $sourceName, $sourceTypeTitle, $sourceTypeName, $sourceID);
 			//this query returnt he next rank after the last insert of images.
-			echo $query;
+			//echo $query;
+			
+			$select = $this->getDbTable()->select();
+			$select->from($this->getDbTable(), "coalesce(max(imageOrder),0) + 1")
+			->where('sourceName = ?', $sourceName)
+			->where('sourceTypeTitle = ?', $sourceTypeTitle)
+			->where('sourceTypeName = ?', $sourceTypeName)
+			->where('sourceID = ?', $sourceID);
+			echo $select;
+
 			//$result = ;
-			$result = $this->getDbTable()->fetchAll($query);
-			$row = $result->current();
-			Zend_Debug::dump($row);
+			$result = Zend_Db_Table::getDefaultAdapter()->fetchOne($select);
+			
+			$result2 = $this->getDbTable()->fetchAll($select);
+			//$row = $result->current();
+			Zend_Debug::dump($result);
+			Zend_Debug::dump($result2);
+			
+			Zend_Debug::dump($this->getDbTable());
 			//return $this->loadByQuery($query);
 	}
 	
